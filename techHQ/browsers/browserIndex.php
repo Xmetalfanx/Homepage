e49a71199b24aaefdf28d8850c0436a418d92a29
Xmetalfanx <Xmetalfanx@yahoo.com>
2024-11-22 03:37:44 -0500

    Browser frontpage refactoring - Mass
    - Update for browser frontpage on smaller layouts

--- techHQ/browsers/browserIndex.php
+++ techHQ/browsers/browserIndex.php
@@ -20,43 +20,85 @@
 
 
 <div id="browser-frontpage">
-  <div id="browsers">
     <h3>Browsers</h3>
     <div id="browser-engines-container">
-      <div>
-        <h4>Chromium based - Blink Engine</h4>
-        <ul>
-          <li class="btn"><a href="https://www.google.com/chrome/index.html" target="_blank"><img class="icon" src="/techHQ/progIcons/internet/browsers/chrome.svg" alt="Google Chrome SVG Icon">Google Chrome</a></li>
-          <li class="btn"><a href="https://brave.com/" target="_blank"><img class="icon" src="/techHQ/progIcons/internet/browsers/brave.png" alt="Brave Browser Icon">Brave</a></li>
-          <li class="btn"><a href="https://vivaldi.com/" target="_blank"><img class="icon" src="/techHQ/progIcons/internet/browsers/vivaldi.svg" alt="Vivaldi Browser SVG Icon">Vivaldi</a></li>
-          <li class="btn"><a href="https://www.microsoft.com/en-us/edge/download?form=MA13FJ" target="_blank"><img class="icon" src="/techHQ/progIcons/internet/browsers/edge.png" alt="Microsoft Edge icon">Micrsoft Edge</a></li>
-          <li class="btn"><a href="https://github.com/ungoogled-software/ungoogled-chromium" target="_blank"><img class="icon" src="/techHQ/progIcons/internet/browsers/chromium.png" alt="Chromium/Ungoogle Chromium icon">Ungoogled Chromium</a></li>
-        </ul>
+
+      <div class="card">
+        <div class="card-block">
+          <div class="card-title">
+            <h4>Chromium based - Blink Engine</h4>
+          </div>
+          <div class="card-body">
+            <ul>
+                <li>
+                  <a href="https://brave.com/" target="_blank">
+                    <img class="icon" src="/techHQ/progIcons/internet/browsers/brave.png" alt="Brave Browser Icon">Brave</a>
+                </li>
+                <li>
+                    <a href="https://www.google.com/chrome/index.html" target="_blank">
+                      <img class="icon" src="/techHQ/progIcons/internet/browsers/chrome.svg" alt="Google Chrome SVG Icon">Google Chrome</a>
+                </li>
+                <li>
+                  <a href="https://www.microsoft.com/en-us/edge/download?form=MA13FJ" target="_blank">
+                    <img class="icon" src="/techHQ/progIcons/internet/browsers/edge.png" alt="Microsoft Edge icon">Micrsoft Edge</a></li>
+                <li>
+                  <a href="https://github.com/ungoogled-software/ungoogled-chromium" target="_blank">
+                    <img class="icon" src="/techHQ/progIcons/internet/browsers/chromium.png" alt="Chromium/Ungoogle Chromium icon">Ungoogled Chromium</a>
+                  </li>
+                <li>
+                  <a href="https://vivaldi.com/" target="_blank">
+                    <img class="icon" src="/techHQ/progIcons/internet/browsers/vivaldi.svg" alt="Vivaldi Browser SVG Icon">Vivaldi</a>
+                </li>
+              </ul>
+          </div>
+        </div>
       </div>
-      <div>
-        <h4>Firefox Based - Geck Engine</h4>
-        <ul>
-          <li class="btn"><a href="https://www.mozilla.org/en-US/firefox/new/" target="_blank"><img class="icon" src="/techHQ/progIcons/internet/browsers/firefox.svg" alt="Firefox SVG icon">Firefox</a></li>
-          <li class="btn"><a href="https://librewolf.net/" target="_blank"><img class="icon" src="/techHQ/progIcons/internet/browsers/librewolf.png" alt="LibreWolf Icon">LibreWolf</a></li>
-        </ul>
+
+      <div class="card">
+        <div class="card-block">
+          <div class="card-title"><h4>Firefox Based - Gecko Engine</h4></div>
+          <div class="card-body">
+            <ul>
+                <li>
+                  <a href="https://www.mozilla.org/en-US/firefox/new/" target="_blank">
+                    <img class="icon" src="/techHQ/progIcons/internet/browsers/firefox.svg" alt="Firefox SVG icon">Firefox</a>
+                  </li>
+                <li>
+                  <a href="https://librewolf.net/" target="_blank">
+                    <img class="icon" src="/techHQ/progIcons/internet/browsers/librewolf.png" alt="LibreWolf Icon">LibreWolf</a>
+                  </li>
+              </ul>
+
+          </div>
+        </div>
       </div>
-      <div>
-        <h4>Firefox Based - Goanna Engine</h4>
-        <ul>
-          <li class="btn"><a href="https://www.basilisk-browser.org/" target="_blank"><img class="icon" src="/techHQ/progIcons/internet/browsers/basilisk.png" alt="Basilisk icon">Basilisk</a></li>
-          <li class="btn"><a href="https://www.palemoon.org/" target="_blank"><img class="icon" src="/techHQ/progIcons/internet/browsers/pm_logo.png" alt="Pale Moon Icon">Pale Moon</a></li>
-        </ul>
+
+      <div class="card">
+        <div class="card-block">
+          <div class="card-title"> <h4>Firefox Based - Goanna Engine</h4>    </div>
+          <div class="card-body">
+            <ul>
+              <li>
+                <a href="https://www.basilisk-browser.org/" target="_blank">
+                  <img class="icon" src="/techHQ/progIcons/internet/browsers/basilisk.png" alt="Basilisk icon">Basilisk</a>
+                </li>
+              <li>
+                <a href="https://www.palemoon.org/" target="_blank">
+                  <img class="icon" src="/techHQ/progIcons/internet/browsers/pm_logo.png" alt="Pale Moon Icon">Pale Moon</a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
   <div id="total-addons">
     <h3>Browser Addons</h3>
     <div id="addons-container">
       <div class="addon">
         <img class="icon" src="/techHQ/browsers/graphics/icons/ublock-origin.svg" alt="Ublock Origin SVG Icon">
         <div class="fw-800">Ublock Origin</div>
-        <div class="addon-descript">Ad/Content Blocker</div>
-        <ul class="browsers">
+        <div class="fs-italics my-2">Ad/Content Blocker</div>
+        <ul class="d-flex">
           <li><a href="https://addons.mozilla.org/en-US/firefox/addon/ublock-origin/"><img class="icon" src="/techHQ/progIcons/internet/browsers/firefox.svg" alt="Ublock Origin Firefox"></a></li>
           <li><a href="https://chrome.google.com/webstore/detail/ublock-origin/cjpalhdlnbpafiamejdnhcphjbkeiagm" target="_blank"><img class="icon" src="/techHQ/progIcons/internet/browsers/chrome.svg" alt="Ublock Origin Chrome Store"></a></li>
           <li><a href="https://github.com/gorhill/uBlock" target="_blank"><img class="icon" src="/graphics/simpleIcons/github.svg" alt="Ublock Origin Github"></a></li>
@@ -65,8 +107,8 @@
       <div class="addon">
         <img class="icon" src="/techHQ/browsers/graphics/icons/bit-warden.svg" alt="Bit Warden SVG Icon">
         <div class="fw-800">Bit Warden</div>
-        <div class="addon-descript">Open-Source Password Manager</div>
-        <ul class="browsers">
+        <div class="fs-italics my-2">Open-Source Password Manager</div>
+        <ul class="d-flex">
           <li><a href="https://addons.mozilla.org/en-US/firefox/addon/bitwarden-password-manager/" target="_blank"><img class="icon" src="/techHQ/progIcons/internet/browsers/firefox.svg" alt="Bit Warden Firefox"></a></li>
           <li><a href="https://chrome.google.com/webstore/detail/bitwarden-free-password-m/nngceckbapebfimnlniiiahkandclblb" target="_blank"><img class="icon" src="/techHQ/progIcons/internet/browsers/chrome.svg" alt="Bit Warden Chrome Store"></a></li>
           <li><a href="https://github.com/bitwarden/clients" target="_blank"><img class="icon" src="/graphics/simpleIcons/github.svg" alt="Bit Warden Github"></a></li>
