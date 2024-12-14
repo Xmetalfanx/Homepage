c394f87ecee65d7a2c24912c9236e3ff5d292833
Xmetalfanx <Xmetalfanx@yahoo.com>
2024-12-14 00:15:47 -0500

    Section-title syntax fix

--- modules/siteSection.php
+++ modules/siteSection.php
@@ -1,7 +1,7 @@
 
 <div class="site-sections">
   <section>
-    <div class="section--title"><a href="/linuxHQ/index.php">LinuxHQ Section</a></div>
+    <div class="section-title"><a href="/linuxHQ/index.php">LinuxHQ Section</a></div>
     <div class="section--details">
       <div class="section-description">Linux Distribution and Desktop information, including: 
         <ul>
@@ -18,7 +18,7 @@
     </div>
   </section>
   <section>
-    <div class="section--title"><a href="/techHQ/index.php">TechHQ Section</a></div>
+    <div class="section-title"><a href="/techHQ/index.php">TechHQ Section</a></div>
     <div class="section--details">
       <div class="section--image"><img class="mx-auto" src="/graphics/unsplash/christmas/james-wheeler-faMVPE56PnU-unsplash.jpg" alt="Christmas house decoration image">
         <div class="credits">Photo by <a href="https://unsplash.com/@souvenirpixels?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">James Wheeler</a> on <a href="https://unsplash.com/photos/snowman-and-santa-claus-near-house-decor-faMVPE56PnU?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a></div>
