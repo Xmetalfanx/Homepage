7f6df1696d5d7e7316360e4604a68b11cd44bb76
Xmetalfanx <Xmetalfanx@yahoo.com>
2025-01-13 21:29:40 -0500

    removed Sabyaon (apparently discontinued)

--- linuxHQ/distros/gentoo/sabayon.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$pageTitle = 'Sabayon Linux';
-
-    include __DIR__ . "/../../../templates/linux/baseLinuxDistro.tpl";
-?>
-
-<script type="text/javascript">
-	var localDistroName = "sabayon";
-</script>
